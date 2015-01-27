60b2920a9b08cb7508d3263091e55e8794555004
eljoaquin <eljoaquin27@gmail.com>
2015-01-27 16:21:05 +0000

    first baner

--- js/folder.js
+++ js/folder.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
 
 
+    var firstSliderRight = $('.container').width();
+    $('#first-slider').css('right', firstSliderRight);
+    $('#first-slider').show();
 
-    $(".first-spot .box").css('left', $(".first-spot").width());
+    var firstSpotBoxRight = $(".first-spot").width();
+    $(".first-spot .box").css('left', firstSpotBoxRight);
     $(".first-spot .box").show();
     $(".first-spot .box").animate({
         left: 0
-    }, 500, "linear", function() {
+    }, 200, "linear", function() {});
 
+    $(".first-spot").on('click', function() {
+        $("#first-slider").animate({
+            right: 0
+        }, 500, "linear", function() {
+            $(".first-spot .box").css('left', firstSpotBoxRight);
+        });
     });
+
+    $("#first-slider").on('click', function() {
+        $('#first-slider').css('right', firstSliderRight);
+        $(".first-spot .box").animate({
+            left: 0
+        }, 200, "linear", function() {});
+    })
 });
