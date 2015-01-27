e1cc2e17facca18f0613097546162193860527b5
eljoaquin <eljoaquin27@gmail.com>
2015-01-27 18:04:31 +0000

    basic support for all baners

--- js/folder.js
+++ js/folder.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
     $('#first-slider').show();
     $('#second-slider').css('right', sliderRight);
     $('#second-slider').show();
+    $('#third-slider').css('right', sliderRight);
+    $('#third-slider').show();
+    $('#fourth-slider').css('right', sliderRight);
+    $('#fourth-slider').show();
 
     var firstSpotBoxRight = $(".first-spot").width();
     $(".first-spot .box").css('left', firstSpotBoxRight);
@@ -21,6 +25,20 @@ $(document).ready(function() {
         right: 0
     }, 200, "linear", function() {});
 
+    var thirdSpotBoxTop = $(".third-spot").width();
+    $(".third-spot .box").css('bottom', thirdSpotBoxTop);
+    $(".third-spot .box").show();
+    $(".third-spot .box").animate({
+        bottom: 0
+    }, 200, "linear", function() {});
+
+    var fourthSpotBoxTop = $(".fourth-spot").width();
+    $(".fourth-spot .box").css('left', fourthSpotBoxTop);
+    $(".fourth-spot .box").show();
+    $(".fourth-spot .box").animate({
+        left: 0
+    }, 200, "linear", function() {});
+
 
 
     $(".first-spot").on('click', function() {
@@ -29,17 +47,44 @@ $(document).ready(function() {
         }, 500, "linear", function() {
             $(".first-spot .box").css('left', firstSpotBoxRight);
             $(".second-spot .box").css('right', secondSpotBoxTop);
+            $(".third-spot .box").css('bottom', thirdSpotBoxTop);
+            $(".fourth-spot .box").css('left', fourthSpotBoxTop);
         });
     });
-    
+
     $(".second-spot").on('click', function() {
         $("#second-slider").animate({
             right: 0
         }, 500, "linear", function() {
             $(".first-spot .box").css('left', firstSpotBoxRight);
             $(".second-spot .box").css('right', secondSpotBoxTop);
+            $(".third-spot .box").css('bottom', thirdSpotBoxTop);
+            $(".fourth-spot .box").css('left', fourthSpotBoxTop);
         });
     });
+
+    $(".third-spot").on('click', function() {
+        $("#third-slider").animate({
+            right: 0
+        }, 500, "linear", function() {
+            $(".first-spot .box").css('left', firstSpotBoxRight);
+            $(".second-spot .box").css('right', secondSpotBoxTop);
+            $(".third-spot .box").css('bottom', thirdSpotBoxTop);
+            $(".fourth-spot .box").css('left', fourthSpotBoxTop);
+        });
+    });
+    
+    $(".fourth-spot").on('click', function() {
+        $("#fourth-slider").animate({
+            right: 0
+        }, 500, "linear", function() {
+            $(".first-spot .box").css('left', firstSpotBoxRight);
+            $(".second-spot .box").css('right', secondSpotBoxTop);
+            $(".third-spot .box").css('bottom', thirdSpotBoxTop);
+            $(".fourth-spot .box").css('left', fourthSpotBoxTop);
+        });
+    });
+
 
     $("#first-slider").on('click', function() {
         $('#first-slider').css('right', sliderRight);
@@ -50,8 +95,14 @@ $(document).ready(function() {
         $(".second-spot .box").animate({
             right: 0
         }, 200, "linear", function() {});
+        $(".third-spot .box").animate({
+            bottom: 0
+        }, 200, "linear", function() {});
+        $(".fourth-spot .box").animate({
+            left: 0
+        }, 200, "linear", function() {});
     });
-    
+
     $("#second-slider").on('click', function() {
         $('#second-slider').css('right', sliderRight);
 
@@ -60,6 +111,46 @@ $(document).ready(function() {
         }, 200, "linear", function() {});
         $(".second-spot .box").animate({
             right: 0
+        }, 200, "linear", function() {});
+        $(".third-spot .box").animate({
+            bottom: 0
+        }, 200, "linear", function() {});
+        $(".fourth-spot .box").animate({
+            left: 0
+        }, 200, "linear", function() {});
+    });
+
+    $("#third-slider").on('click', function() {
+        $('#third-slider').css('right', sliderRight);
+
+        $(".first-spot .box").animate({
+            left: 0
+        }, 200, "linear", function() {});
+        $(".second-spot .box").animate({
+            right: 0
+        }, 200, "linear", function() {});
+        $(".third-spot .box").animate({
+            bottom: 0
+        }, 200, "linear", function() {});
+        $(".fourth-spot .box").animate({
+            left: 0
+        }, 200, "linear", function() {});
+    });
+
+    $("#fourth-slider").on('click', function() {
+        $('#fourth-slider').css('right', sliderRight);
+
+        $(".first-spot .box").animate({
+            left: 0
+        }, 200, "linear", function() {});
+        $(".second-spot .box").animate({
+            right: 0
+        }, 200, "linear", function() {});
+        $(".third-spot .box").animate({
+            bottom: 0
+        }, 200, "linear", function() {});
+        $(".fourth-spot .box").animate({
+            left: 0
         }, 200, "linear", function() {});
     });
 });
